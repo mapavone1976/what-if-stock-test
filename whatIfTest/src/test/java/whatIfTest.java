f1f5531194ad1d5b319e6f5844cec618f824ad56
Miguel <mapavone@yahoo.com>
2019-05-14 16:29:43 -0300

    V2

--- whatIfTest/src/test/java/whatIfTest.java
+++ whatIfTest/src/test/java/whatIfTest.java
@@ -1,64 +1,162 @@
+import com.opencsv.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
-
 import static io.restassured.RestAssured.given;
-
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class whatIfTest {
 
+    private static final int POSCOLCLOSE = 4;
+    private static final String PERMISSIONFAILEDCODE = "QEPx04";
+    private static final String SYMBOLINCORRECTCODE = "QECx02";
+
     @Test
     public void whatIfTest_M() {
-        RestAssured.baseURI ="https://stock-whatif.herokuapp.com/whatif";
 
-        Response response = given().
-                contentType("application/json").
-                accept("application/jason").
-            when().
-                get("/AAPL").
-            then().
-                statusCode(200).
-                assertThat().body("high.date", equalTo( "2018-10-03") ).
-                assertThat().body("high.price", equalTo( "232.07") ).
-                assertThat().body("low.date", equalTo( "2019-01-03") ).
-                assertThat().body("low.price", equalTo( "142.19") ).
-                extract().
-                response();
+        //Positive Paths
+        TestSymbol("AAPL");
+        TestSymbol("HD");
+        TestSymbol("MSFT");
+        TestSymbol("FB");
 
-        response.print();
+        //Negative Path
+        TestNegativePath("Test1");
+        TestNegativePath("Test2");
 
-        Response response2 = given().
-                contentType("application/json").
-                accept("application/jason").
-                when().
-                get("/HD").
-                then().
-                statusCode(200).
-                assertThat().body("high.date", equalTo( "2018-09-11") ).
-                assertThat().body("high.price", equalTo( "213.85") ).
-                assertThat().body("low.date", equalTo( "2018-12-24") ).
-                assertThat().body("low.price", equalTo( "158.14") ).
-                extract().
-                response();
 
-        response2.print();
+    }
 
-        Response response3 = given().
-                contentType("application/json").
-                accept("application/jason").
-                when().
-                get("/MSFT").
-                then().
-                statusCode(200).
-                assertThat().body("high.date", equalTo( "2019-04-30") ).
-                assertThat().body("high.price", equalTo( "130.6") ).
-                assertThat().body("low.date", equalTo( "2018-02-08") ).
-                assertThat().body("low.price", equalTo( "85.01") ).
-                extract().
-                response();
+    private void TestSymbol(String xSymbol) {
+        try {
+            //Declaring variables
+            String sMaxDate; String sMaxValue; int sMaxIndex;
+            String sMinDate; String sMinValue; int sMinIndex;
 
-        response3.print();
+            //Reading CSV File
+            RestAssured.baseURI ="https://stock-whatif.herokuapp.com/whatif";
+            Response response = given().contentType("text").when().get("/" + xSymbol + "/raw").then().statusCode(200).
+                    extract().response();
+
+            if (response.asString().indexOf(SYMBOLINCORRECTCODE) != -1 ) {
+                throw new IOException( "The symbol doesn't exists " + xSymbol);
+            }
+
+            if (response.asString().indexOf(PERMISSIONFAILEDCODE) != -1 ) {
+                throw new IOException("You don have permissions to see the symbol " + xSymbol);
+            }
+
+            InputStreamReader readerCSV = new InputStreamReader(response.asInputStream()) ;
+
+            // create csvParser and CSVReader objects
+            CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
+            CSVReader csvReader = new CSVReaderBuilder(readerCSV).withCSVParser(parser).withSkipLines(1).build();
+
+            // Read all data at once from CSV
+            List<String[]> allData = csvReader.readAll();
+
+            //Getting Max & Min
+            sMaxIndex = getMaxLine(allData);
+            //sMaxDate = new SimpleDateFormat("yyyy-MM-dd").parse( allData.get(sMaxIndex)[0]);
+            sMaxDate = allData.get(sMaxIndex)[0];
+            //sMaxValue = Double.parseDouble(allData.get(sMaxIndex)[POSCOLCLOSE]);
+            sMaxValue = allData.get(sMaxIndex)[POSCOLCLOSE];
+
+            sMinIndex = getMinLine(allData);
+            //sMinDate = new SimpleDateFormat("yyyy-MM-dd").parse( allData.get(sMinIndex)[0]);
+            sMinDate = allData.get(sMinIndex)[0];
+            //sMinValue = Double.parseDouble(allData.get(sMinIndex)[POSCOLCLOSE]);
+            sMinValue = allData.get(sMinIndex)[POSCOLCLOSE];
+
+            //Checking API Return
+            given().contentType("application/json").accept("application/jason").when().get("/" + xSymbol).then().
+                    statusCode(200).
+                    assertThat().body(matchesJsonSchemaInClasspath("Template.json")).   //All data types are validated in JSon format template
+                    assertThat().body("high.date", equalTo(sMaxDate) ).
+                    assertThat().body("high.price", equalTo(sMaxValue) ).
+                    assertThat().body("low.date", equalTo( sMinDate) ).
+                    assertThat().body("low.price", equalTo( sMinValue) );
+
+            //Checking API Return - Reinvest - I include it just to show the behavior reported un Bug #2.
+            given().contentType("application/json").accept("application/jason").when().get("/" + xSymbol + "?reinvest=true").then().
+                    statusCode(200).
+                    assertThat().body(matchesJsonSchemaInClasspath("Template.json")).   //All data types are validated in JSon format template
+                    assertThat().body("high.date", equalTo(sMaxDate) ).
+                    assertThat().body("high.price", equalTo(sMaxValue) ).
+                    assertThat().body("low.date", equalTo( sMinDate) ).
+                    assertThat().body("low.price", equalTo( sMinValue) );
+
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void TestNegativePath(String xSymbol) {
+        try {
+
+            RestAssured.baseURI ="https://stock-whatif.herokuapp.com/whatif";
+
+            //Checking API Return
+            given().contentType("application/json").accept("application/jason").when().get("/" + xSymbol).then().
+                    statusCode(200).
+                    assertThat().body(matchesJsonSchemaInClasspath("Template.json"))  ; //All data types are validated in JSon format template
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getMaxLine (List<String []> xCSV) {
+        int i;
+        int iMax;
+        double sVAC;
+        double sMaxVAC;
+
+        i=0;
+        sMaxVAC = 0;
+        iMax = 0;
+        for (String[] row : xCSV) {
+            sVAC = Double.parseDouble( row[POSCOLCLOSE]);
+            if (sVAC > sMaxVAC ) {
+                iMax    = i;
+                sMaxVAC = sVAC;
+            }
+
+            i++;
+        }
+
+        return iMax;
+
+    }
+
+    private int getMinLine (List<String []> xCSV) {
+        int i;
+        int iMin;
+        double sVAC;
+        double sMinVAC;
+
+        i=0;
+        sMinVAC = 999999;
+        iMin = 9999999;
+        for (String[] row : xCSV) {
+            sVAC = Double.parseDouble( row[POSCOLCLOSE]);
+            if (sVAC < sMinVAC ) {
+                iMin    = i;
+                sMinVAC = sVAC;
+            }
+
+            i++;
+        }
+
+        return iMin;
 
     }
 
